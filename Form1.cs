9bbba13ba347f9dc3a464e9478f80626c4e2062d
ManuAlexandru <92298889+ManuAlexandru@users.noreply.github.com>
2022-07-06 19:38:28 +0300

    Preject added

---
+++ Form1.cs
@@ -0,0 +1,274 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Cronometru4
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        SqlConnection sqlConn = new SqlConnection();//cu ajutorul sqlConn creez conexiunea si o deschid/inchid
+        SqlCommand sqlCmd = new SqlCommand();//Ma folosesc de sqlCmd pentru comenzi
+        DataTable sqlDt = new DataTable();//stochez datele din Bd in forma tabelara
+        string sqlQuerry;
+        SqlDataReader sqlRd;//Cu sqlRd citesc datele din Db
+        string dataSource = "DESKTOP-3QDGK42\\SQLEXPRESS";//Server ul
+        string initialCatalog = "Timer";//Baza de date ul.
+        Boolean integratedSecurity=true;//conectarea la Microsoft Sql Server
+       public Color culoareButoane;
+        public Color culoareBacKground;
+        string date = DateTime.UtcNow.ToString("MM-dd-yyyy");
+        public int designTimer = Properties.Settings.Default.AppTimerSetting;//Pentru UnitTest
+        public Color designBackground = Properties.Settings.Default.AppColorBackground1;
+        public Color designBackgroundBtn = Properties.Settings.Default.AppColor;
+       
+                 
+
+        private FormSettings formSettings;
+        
+
+        public int secondsCount = 0;//numarul de secunde
+        public int secondsCountPause=0;//numarul de secunde de pauza
+        Timer disTmr = new Timer();// Declar global Timer ul
+        Timer disTmrPauza = new Timer();//Timer ul pentru pauza
+        public Form()
+        {
+            InitializeComponent();
+            panouAfisare.Visible = false;
+           
+            UploadData();
+           
+
+
+
+
+        }
+
+        
+
+        private void UploadData()
+        {
+            sqlConn.ConnectionString = "Data Source=" + dataSource + "; Initial Catalog=" + initialCatalog + "; Integrated Security=" + integratedSecurity + ";";
+            sqlConn.Open();
+            sqlCmd.Connection = sqlConn;
+          sqlCmd.CommandText = "SELECT * FROM timer2";
+            sqlRd = sqlCmd.ExecuteReader();
+            sqlDt.Load(sqlRd);
+            sqlRd.Close();
+            sqlConn.Close();
+            afisareDate.DataSource = sqlDt;
+
+
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void disTmr_Tick(object sender, EventArgs e)//Eventul prin care numar secundele si le transpun in format hh:mm:ss
+        {
+            if (Properties.Settings.Default.AppTimerSetting == 1)
+            {
+                secondsCount++;
+                TimeSpan time = new TimeSpan(0, 0, secondsCount - secondsCountPause);
+                // secondsBlock.Text = time.ToString() + " secunde";
+                afisareSecunde.Text = time.ToString();
+            }
+            else
+            {
+                secondsCount++;
+                TimeSpan time = new TimeSpan(0, 0, secondsCount);
+                //secondsBlock.Text = time.ToString() + " secunde";
+                afisareSecunde.Text = time.ToString();
+            }
+
+        }
+        public void disTmr_TickPauza(object sender, EventArgs e)//Eventul prin care numar secundele si le transpun in format hh:mm:ss
+        {if(Properties.Settings.Default.AppTimerSetting == 1)
+            {
+                secondsCountPause++;
+            }
+            else
+
+            {
+                secondsCountPause++;
+                TimeSpan timePause = new TimeSpan(0, 0, secondsCountPause);
+                //secondsPause.Text = timePause.ToString() + " secunde";
+                afisareSecundePauza.Text = timePause.ToString();
+
+            }
+           
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            if (secondsCount > 0)
+                MessageBox.Show("Hooo!!!!");
+
+            else
+
+
+            {
+                if (Properties.Settings.Default.AppTimerSetting == 1)
+                {
+                    panouAfisare.Visible = true;
+                    disTmr = new Timer();//Creez o noua instanta a obiectului
+                    disTmr.Tick += new EventHandler(disTmr_Tick);
+                    disTmr.Interval = 1000;
+                    disTmr.Start();
+                    afisareSecundePauza.Visible = false;
+                }
+                else
+                {
+                    panouAfisare.Visible = true;
+                    disTmr = new Timer();//Creez o noua instanta a obiectului
+                    disTmr.Tick += new EventHandler(disTmr_Tick);
+                    disTmr.Interval = 1000;
+                    disTmr.Start();
+                    afisareSecundePauza.Visible = true;
+
+
+                }
+                if (btnPauza.Text == "O_Pauza")
+                {
+                    btnPauza.Text = "S_Pauza";
+                    disTmrPauza.Stop();
+
+                }
+                secondsCountPause = 0;
+            }
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            disTmr.Stop();
+           
+           
+
+
+
+            sqlConn.ConnectionString = "Data Source=" + dataSource + "; Initial Catalog=" + initialCatalog + "; Integrated Security=" + integratedSecurity + ";";
+                secondsCount = secondsCount - secondsCountPause;
+
+            try
+            {
+                sqlConn.Open();
+                if (secondsCount < 0)
+                    MessageBox.Show("Uups....A aparut o eroare!");
+
+                sqlQuerry = "insert into timer2 (Timp,Data,Pauza)"+
+                    "values('"+ secondsCount+"','"+ date + "','"+ secondsCountPause +"')";
+                sqlCmd = new SqlCommand(sqlQuerry,sqlConn);
+                sqlRd = sqlCmd.ExecuteReader();
+                sqlConn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            
+            
+            }
+            finally
+            {
+                sqlConn.Close();
+
+            }
+            UploadData();
+            secondsCount = 0;
+            secondsCountPause = 0;
+            TimeSpan timePause = new TimeSpan(0, 0, 0);
+            afisareSecundePauza.Text = timePause.ToString();
+            panouAfisare.Visible = false;
+        }
+
+        private void btnAdmin_Click(object sender, EventArgs e)
+        {
+           FormaEmail  formaEmail = new FormaEmail();
+            formaEmail.Show();
+           
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (btnPauza.Text == "S_Pauza")
+            {
+                btnPauza.Text = "O_Pauza";
+                   disTmrPauza = new Timer();
+                disTmrPauza.Start();
+                disTmrPauza.Tick += new EventHandler(disTmr_TickPauza);
+                disTmrPauza.Interval = 1000;
+            }
+            else
+            {
+                btnPauza.Text = "S_Pauza";
+                disTmrPauza.Stop();
+                
+
+            }
+
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+             formSettings = new FormSettings(this);
+            formSettings.Visible = true;
+        }
+
+       
+       
+       
+        public void SetareCuloare0()
+        {
+
+
+
+
+
+           
+                btnStart.BackColor = culoareButoane;
+            btnAdmin.BackColor = culoareButoane;
+            btnPauza.BackColor = culoareButoane;
+            btnSettings.BackColor = culoareButoane;
+            btnStop.BackColor = culoareButoane;
+            panel1.BackColor = culoareBacKground;
+
+            this.Update();
+
+
+
+                
+            
+
+
+
+
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            culoareButoane = Properties.Settings.Default.AppColor;
+            culoareBacKground = Properties.Settings.Default.AppColorBackground1;
+            SetareCuloare0();
+        }
+
+       
+    }
+}
---
+++ FormaEmail.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Cronometru4
+{
+    partial class FormaEmail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(349, 309);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(263, 160);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(262, 23);
+            this.txtSubject.TabIndex = 1;
+            this.txtSubject.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(263, 200);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(262, 88);
+            this.txtText.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(263, 109);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(262, 23);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Send To";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Subject:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Text:";
+            // 
+            // FormaEmail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.btnSend);
+            this.Name = "FormaEmail";
+            this.Text = "FormaEmail";
+            this.Load += new System.EventHandler(this.FormaEmail_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button btnSend;
+        public System.Windows.Forms.TextBox txtSubject;
+        public System.Windows.Forms.TextBox txtText;
+        public System.Windows.Forms.TextBox txtEmail;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label3;
+    }
+}
